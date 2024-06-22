3593966b1d263669cc3edad66f1fd23f7155bfa3
Cauan <cauanvictor0325@gmail.com>
2024-06-22 12:01:46 -0300

    Quase la no form do header

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,17 @@ function Header({children}) {
     )
 }
 
-function HeaderLink({href, children}) {
+function FormBase() {
+    return (
+        <form action="#" method="POST">
+            <div class="mt-2.5">
+                <input type="text" name="first-name" id="first-name" class="block rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+            </div>
+        </form>
+    )
+}
+
+function HeaderSearch() {
     return (
         <a href={href} className="font-semibold leading-6 ml-5 text-black">{children}</a>
     )
@@ -18,17 +28,7 @@ function HeaderLink({href, children}) {
 function App() {
     return (
         <>
-            <Header>
-                <HeaderLink 
-                    href="https://company">
-                    Company
-                </HeaderLink>
-                <HeaderLink 
-                    href="https://marketplace">Marketplace</HeaderLink>
-                <HeaderLink> 
-                    Teste
-                </HeaderLink>
-            </Header>
+            <FormBase></FormBase>
            
         </>
     );
