e4d565d83979c764d9ec1ae5b6d37201d835bd7f
Cauan <cauanvictor0325@gmail.com>
2024-06-22 11:44:44 -0300

    Criando components para o menu do cabeçalho

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,37 @@
-import { useState } from 'react'
+
+function Header({children}) {
+    return (
+        <header className="bg-white">
+            <nav className="mx-auto flex items-center p-6 shadow-md">
+                {children}
+            </nav>
+        </header>
+    )
+}
+
+function HeaderLink({href, children}) {
+    return (
+        <a href={href} className="font-semibold leading-6 ml-5 text-black">{children}</a>
+    )
+}
 
 function App() {
-  return (
-    <h1 className="text-5xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+    return (
+        <>
+            <Header>
+                <HeaderLink 
+                    href="https://company">
+                    Company
+                </HeaderLink>
+                <HeaderLink 
+                    href="https://marketplace">Marketplace</HeaderLink>
+                <HeaderLink> 
+                    Teste
+                </HeaderLink>
+            </Header>
+           
+        </>
+    );
 }
 
 export default App
