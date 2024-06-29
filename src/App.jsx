be131ff2da8a40914ec5ef6d19f6ca865a61a02c
Kelvys Moura <kelvysmoura2@gmail.com>
2024-06-29 09:34:32 -0300

    Concluindo cabeçalho

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,11 @@ import FormBase from './components/FormBase';
 
 function App() {
     return (
-        <Header className="bg-blue-500 flex p-3">
-            <h1 className="ml-5">FS34Blog</h1>
+        <Header className="flex items-center p-3 shadow-md">
+            <h1 className="ml-5 font-bold text-2xl">FS34Blog</h1>
             <FormBase className="ml-5">
-                <input type="text" />
+                <input type="text" className="border px-2 py-1 rounded" />
+                <button className="border rounded border-black px-2 py-1 ml-2">Pesquisar</button>
             </FormBase>
         </Header>
     );
