b4e88e7a3418e9d72f443f6ac1d4ad6dfad81c1b
Kelvys Moura <kelvysmoura2@gmail.com>
2024-07-20 09:24:38 -0300

    Corrigindo AuthorPage com as novas apis

--- src/pages/AuthorPage.jsx
+++ src/pages/AuthorPage.jsx
@@ -3,18 +3,19 @@ import Layout from "../components/Layout";
 import Post from '../components/Post';
 import {useParams} from 'react-router-dom';
 import posts from '../api/posts';
+import users from '../api/users';
 
 export default function AuthorPage () {
 
   let params = useParams();
 
-  let post = posts.find(function (post) {
-    return post.author.username === params.username;
+  let user = users.find(function (user) {
+    return user.username === params.username;
   });
 
   return (
     <Layout>
-        <h1>Author: {post.author.name}</h1>
+        <h1>Author: {user.name}</h1>
     </Layout>
   );
 }
